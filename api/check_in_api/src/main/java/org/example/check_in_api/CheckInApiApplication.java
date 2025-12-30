92824e52711bb82d543ac2d88c36720792e6c301
OchianIulian <aochian@bandwidth.com>
2025-12-30 13:43:23 +0200

    Create initial projects fe + be

---
+++ api/check_in_api/src/main/java/org/example/check_in_api/CheckInApiApplication.java
@@ -0,0 +1,13 @@
+package org.example.check_in_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckInApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckInApiApplication.class, args);
+    }
+
+}
---
+++ api/check_in_api/src/test/java/org/example/check_in_api/CheckInApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.check_in_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckInApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
